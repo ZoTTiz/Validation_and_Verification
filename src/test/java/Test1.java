b4d9fdf3c9d600a080bfe0f494c4498f04ca532c
Bruno Biz <brunobizdc@gmail.com>
2022-05-08 19:46:17 -0300

    Teste login invalido no Moodle

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,4 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -9,12 +12,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.v85.indexeddb.model.Key;
 
 public class Test1 {
+    WebDriver driver;
+
+    /*@BeforeAll
+    public void setUpAll(){
+    }*/
+
+    @BeforeEach
+    public void setUpEach() {
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
     @Test
     @DisplayName("Pesquisa no Google, para ver se abre o site escolhido")
     public void test1(){
-        System.setProperty("webdriver.chrome.driver", "C:/Users/Antonio/chromedriver_win32/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
         driver.get("https://www.google.com/");
 
         WebElement search = driver.findElement(By.name("q"));
@@ -30,4 +42,27 @@ public class Test1 {
 
         //driver.close();
     }
+
+    @Test
+    @DisplayName("Login invalido no MOODLE")
+    public void test2(){
+        driver.get("https://moodle.utfpr.edu.br/login/index.php");
+
+        WebElement user = driver.findElement(By.name("username"));
+        user.sendKeys(    "aINVALIDO" + Keys.ENTER);
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys(    "INVALIDO" + Keys.ENTER);
+
+        String urlCurrent = driver.getCurrentUrl();
+
+        WebElement btnLogIN = driver.findElement(By.id("loginbtn"));
+        btnLogIN.click();
+
+        String expectedURL = driver.getCurrentUrl();
+
+        assertEquals(expectedURL, urlCurrent);
+        //driver.close();
+    }
 }
+
