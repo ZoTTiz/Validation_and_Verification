570d9762ac146431df60d386e66c683d472290bd
ZoTTiz <antonio.ls.zotti@gmail.com>
2022-05-10 15:24:00 -0300

    Finalizado.

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -14,10 +14,6 @@ import org.openqa.selenium.devtools.v85.indexeddb.model.Key;
 public class Test1 {
     WebDriver driver;
 
-    /*@BeforeAll
-    public void setUpAll(){
-    }*/
-
     @BeforeEach
     public void setUpEach() {
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
@@ -25,41 +21,78 @@ public class Test1 {
     }
 
     @Test
-    @DisplayName("Pesquisa no Google, para ver se abre o site escolhido")
+    @DisplayName("Pesquisa no Google, para ver se abre o site escolhido.")
     public void test1(){
         driver.get("https://www.google.com/");
 
         WebElement search = driver.findElement(By.name("q"));
         search.sendKeys(    "zotti" + Keys.ENTER);
 
-       // driver.findElement(By.linkText("Telefone")).click();
         driver.findElement(By.xpath("//a[@href='https://www.zotti.ind.br/']")).click();
 
         String urlCurrent = driver.getCurrentUrl();
         String expectedURL = "https://www.zotti.ind.br/";
 
         assertEquals( expectedURL, urlCurrent);
-
         //driver.close();
     }
 
     @Test
-    @DisplayName("Login invalido no MOODLE")
+    @DisplayName("Pesquisa no Google uma imagem e ao mesmo tempo tenta selecionar a mesma e abrir o link da página.")
     public void test2(){
+        driver.get("https://www.google.com/");
+
+        //driver.findElement(By.xpath("//a[@href='https://www.zotti.ind.br/']")).click();
+        driver.findElement(By.xpath("//a[@href='https://www.google.com.br/imghp?hl=pt-BR&tab=ri&authuser=0&ogbl/']")).click();
+
+        WebElement search = driver.findElement(By.name("q"));
+        search.sendKeys(    "software" + Keys.ENTER);
+
+        driver.findElement(By.className("n3VNCb")).click();
+        driver.findElement(By.xpath("//a[@href='https://canaltech.com.br/software/o-que-e-software/']")).click();
+
+        String urlCurrent = driver.getCurrentUrl();
+        String expectedURL = "https://canaltech.com.br/software/o-que-e-software/";
+
+        assertEquals(expectedURL, urlCurrent);
+        //driver.close();
+    }
+
+    @Test
+    @DisplayName("Acessar a página doodles no google.")
+    public void test3(){
+        driver.get("https://www.google.com/");
+
+        driver.findElement(By.className("M6CB1c rr4y5c")).click();
+
+        driver.findElement(By.name("btnI")).click();
+
+        driver.findElement(By.id("about-link")).click();
+
+        String urlCurrent = driver.getCurrentUrl();
+        String expectedURL = "https://www.google.com/doodles/about";
+
+        assertEquals(expectedURL, urlCurrent);
+        //driver.close();
+    }
+
+    @Test
+    @DisplayName("Login inválido no MOODLE.")
+    public void test4(){
         driver.get("https://moodle.utfpr.edu.br/login/index.php");
 
         WebElement user = driver.findElement(By.name("username"));
-        user.sendKeys(    "aINVALIDO" + Keys.ENTER);
+        user.sendKeys(    "aINVALIDO");
 
         WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys(    "INVALIDO" + Keys.ENTER);
+        password.sendKeys(    "INVALIDO");
 
         String urlCurrent = driver.getCurrentUrl();
 
         WebElement btnLogIN = driver.findElement(By.id("loginbtn"));
         btnLogIN.click();
 
-        String expectedURL = driver.getCurrentUrl();
+        String expectedURL = "https://moodle.utfpr.edu.br/my/";
 
         assertEquals(expectedURL, urlCurrent);
         //driver.close();
